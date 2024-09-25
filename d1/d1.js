d8b01adbc41bbbb171049d39c3df60ed491612e1
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-09-25 10:44:31 +0530

    patterns

---
+++ d1/d1.js
@@ -0,0 +1,24 @@
+var a=""
+
+for(i=1;i<=5;i++){
+    a=""
+     for(j=4;j>=i;j--){
+          a = a+"  "
+     }
+     for(j=1;j<=i;j++){
+        a = a+"   *"
+   }
+         console.log(a)
+        
+ }
+ for(i=4;i>=1;i--){
+    a=""
+     for(j=4;j>=i;j--){
+          a = a+"  "
+     }
+     for(j=1;j<=i;j++){
+        a = a+"   *"
+   }
+         console.log(a)
+        
+ }
