de1f8b6e21d9c77b3d98fc79ed3abd8e54982d4b
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-10-05 10:01:07 +0530

    JAVASCRIPT-PATTERNS

---
+++ patterns/sp1.js
@@ -0,0 +1,16 @@
+var a="" ;
+
+
+for(i=1;i<=4;i++){
+    a=""
+    for(j=1;j<=4;j++){
+        if(j%2==0){
+            a=a+" $ "
+        }
+        else{
+            a=a+" * "
+        }
+    }
+       
+        console.log(a)
+}
