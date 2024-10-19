56f26706fa690e29bbc8558101dca5ee5be23b2f
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-10-19 10:31:54 +0530

    JAVASCRIPT-PATTERNS

---
+++ patterns/tr3.js
@@ -0,0 +1,16 @@
+var a=""
+
+for(i=1;i<=5;i++){
+   a=""
+    for(j=1;j<=i;j++){
+         a = a+" * "
+    }
+        console.log(a)
+}
+for(i=1;i<=5;i++){
+    a=""
+     for(j=4;j>=i;j--){
+          a = a+" * "
+     }
+         console.log(a)
+ }
