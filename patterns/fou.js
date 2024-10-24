009b30508f8bc02695583e32e151b84f91dfc8cd
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-10-24 12:45:06 +0530

    JAVASCRIPT-PATTERNS

---
+++ patterns/fou.js
@@ -0,0 +1,13 @@
+var a=""
+
+ for(i=5;i>=1;i--){
+    a=""
+     for(j=4;j>=i;j--){
+          a = a+"  "
+     }
+     for(j=1;j<=i;j++){
+        a = a+"   *"
+   }
+         console.log(a)
+        
+ }
