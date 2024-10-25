0269a0dc397ef2f876170ae0e38df48b5203534e
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-10-25 10:55:02 +0530

    JAVASCRIPT-PATTERNS

---
+++ patterns/new.js
@@ -0,0 +1,20 @@
+var a=""
+
+for(i=5;i>=0;i--){
+   a=""
+    for(j=1;j<=i;j++){
+         a = a+" * "
+    }
+        console.log(a)
+}
+
+
+
+
+for(i=1;i<=5;i++){
+    a=""
+     for(j=1;j<=i;j++){
+          a = a+" * "
+     }
+         console.log(a)
+ }
